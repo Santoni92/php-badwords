7414ca445339f7a284f8bf8b357d2b713cc416b8
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-02 18:03:06 +0200

    aggiunta istruzione per sostituire la parola da censurare

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
+//phpinfo();
 $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
              Donec fringilla ligula et sem finibus pulvinar. Fusce quis fermentum enim. Vivamus est odio, 
              fringilla ultricies venenatis nec, tincidunt at mi. Etiam mattis nulla arcu, vitae pharetra orci malesuada
               eleifend. Donec sed nibh non nibh auctor gravida et eu est. Vestibulum vitae tortor felis. Quisque arcu massa, 
               egestas vel elit id, dapibus feugiat massa. Duis lacinia, nisi et euismod dapibus, lectus mi ullamcorper lacus, 
               at egestas sem est id dolor. Cras maximus eros ac ligula fermentum sodales. ";
+$lunghezzaParagrafoConSpazi = strlen($paragrafo);
 $paragrafoSenzaSpazi = str_replace(" ","",$paragrafo);
-$lunghezzaParagrafo = strlen($paragrafoSenzaSpazi);
-
+$lunghezzaParagrafoSenzaSpazi = strlen($paragrafoSenzaSpazi);
+$paragrafoSenzaParolaCensurata = str_replace($_GET["parolaDaCensurare"],"***",$paragrafo );
 ?>
 
 <h1>PHP Badwords</h1>
 <p><?php echo($paragrafo) ?></p>
+<p>La lunghezza del paragrafo avente spazi tra le varie parole risulta: <?php echo(strlen($paragrafo))?></p>
 
 <h2>paragrafo senza spazi</h2>
 <p><?php echo($paragrafoSenzaSpazi)?></p>
+<p>La lunghezza del paragrafo senza spazi tra le parole risulta: <?php echo($lunghezzaParagrafoSenzaSpazi)?></p>
 
-<p>La lunghezza del paragrafo risulta: <?php echo($lunghezzaParagrafo)?></p>
+<p>La parola di cui è richiesta la censura risulta: <?php echo($_GET["parolaDaCensurare"]) ?></p>
+<p>Il paragrafo con la parola da censurare sostituita  risulta: <?php echo($paragrafoSenzaParolaCensurata) ?> </p>
 
