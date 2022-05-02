44689616915c020ca747da8bbbc049c48bf33b8a
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-02 16:05:35 +0200

    creato file php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$paragrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+             Donec fringilla ligula et sem finibus pulvinar. Fusce quis fermentum enim. Vivamus est odio, 
+             fringilla ultricies venenatis nec, tincidunt at mi. Etiam mattis nulla arcu, vitae pharetra orci malesuada
+              eleifend. Donec sed nibh non nibh auctor gravida et eu est. Vestibulum vitae tortor felis. Quisque arcu massa, 
+              egestas vel elit id, dapibus feugiat massa. Duis lacinia, nisi et euismod dapibus, lectus mi ullamcorper lacus, 
+              at egestas sem est id dolor. Cras maximus eros ac ligula fermentum sodales. ";
+
+?>
+
+<h1>PHP Badwords</h1>
+
+<p><?php echo($paragrafo) ?></p>
