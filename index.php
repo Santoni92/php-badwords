e77a8d1ad2ce9db13ab12c30e767a536f7507f01
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-05-02 16:51:05 +0200

    usata funzione  che mi restituisce il pragrafo come stringa in cui non sono presenti spazi tra le varie parole ed su questa sctringa utilizzata la funzione per il calcolo della sua lunghezza

--- index.php
+++ index.php
@@ -5,9 +5,16 @@ $paragrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.
               eleifend. Donec sed nibh non nibh auctor gravida et eu est. Vestibulum vitae tortor felis. Quisque arcu massa, 
               egestas vel elit id, dapibus feugiat massa. Duis lacinia, nisi et euismod dapibus, lectus mi ullamcorper lacus, 
               at egestas sem est id dolor. Cras maximus eros ac ligula fermentum sodales. ";
+$paragrafoSenzaSpazi = str_replace(" ","",$paragrafo);
+$lunghezzaParagrafo = strlen($paragrafoSenzaSpazi);
 
 ?>
 
 <h1>PHP Badwords</h1>
-
 <p><?php echo($paragrafo) ?></p>
+
+<h2>paragrafo senza spazi</h2>
+<p><?php echo($paragrafoSenzaSpazi)?></p>
+
+<p>La lunghezza del paragrafo risulta: <?php echo($lunghezzaParagrafo)?></p>
+
